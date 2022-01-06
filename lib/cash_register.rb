169e1d9ed138b63211ce663841ec4a99479421c1
ctninman <89664157+ctninman@users.noreply.github.com>
2022-01-06 16:19:14 +0100

    commit tree ce51d3f66e53e964ff74f14bba2d0c239900c0a0

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total, :discount, :items, :most_recent_addition
+
+  def initialize (discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @most_recent_addition
+
+  end
+
+  def total=(total)
+    @total = total
+  end
+
+  def add_item (title, price, quantity = 1)
+    self.total += price * quantity
+    quantity.times { items << title }
+    @most_recent_addition = price * quantity
+
+  end
+
+  def apply_discount 
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      self.total -= (self.total * @discount / 100)
+      "After the discount, the total comes to $#{self.total}."
+    end
+  end
+
+  bimbo = CashRegister.new (20)
+  bimbo.apply_discount
+
+  def items
+    @items
+    
+  end
+
+  def void_last_transaction
+    self.total -= @most_recent_addition
+
+  end
+
+end
